4ba360bacbf178075e78e525cb2d6951d8ae34e1
bmcandeias <50877834+bmcandeias@users.noreply.github.com>
2020-05-28 00:50:45 +0100

    Template BackOffice

--- routes/web.php
+++ routes/web.php
@@ -18,10 +18,19 @@ Route::get('/', 'SiteController@index');
 Route::get('/home', 'SiteController@index')->name('/');
 Auth::routes();
 
-
 Route::get('/admin', 'SiteController@admin');
 Route::get('/func', 'SiteController@func');
 Route::get('/ger', 'SiteController@ger');
 
 Route::resource('user','UserController');
 
+Route::get('/back', 'SiteController@back');
+Route::get('/icon', 'SiteController@icon');
+Route::get('/map', 'SiteController@map');
+Route::get('/maps', 'SiteController@maps');
+Route::get('/notification', 'SiteController@notification');
+Route::get('/rt', 'SiteController@rt');
+Route::get('/tables', 'SiteController@tables');
+Route::get('/typo', 'SiteController@typo');
+Route::get('/upgrade', 'SiteController@upgarde');
+Route::get('/dash', 'SiteController@dash');
