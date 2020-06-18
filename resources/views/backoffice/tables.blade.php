879da4f18c3e9245014f6df5d702b66dd19bdb35
bmcandeias <50877834+bmcandeias@users.noreply.github.com>
2020-06-18 05:11:34 +0100

    Tabelas

--- resources/views/backoffice/tables.blade.php
+++ resources/views/backoffice/tables.blade.php
@@ -1,12 +1,4 @@
 @extends('layouts.backoffice')
 @section("contentB")
-    <div class="content">
-        <div class="container-fluid">
-            <h3>Pagina Principal</h3>
-            <div class="row">
-                <div class="col-lg-3 col-md-6 col-sm-6">
-
-
-
-
+    @include('backoffice/includes/tabels')
 @endsection
