262dd7d945fab0a3c96733fdb00012b5d4dddbc4
bmcandeias <50877834+bmcandeias@users.noreply.github.com>
2020-07-04 04:13:49 +0100

    Tabelas Final final

--- resources/views/backoffice/includes/sidebar_back.blade.php
+++ resources/views/backoffice/includes/sidebar_back.blade.php
@@ -4,63 +4,15 @@
 
       Tip 2: you can also add an image using data-image tag
   -->
-    <div class="logo"><a href="http://www.creative-tim.com" class="simple-text logo-normal">
-            Creative Tim
+    <div class="logo"><a href="/" class="simple-text logo-normal">
+            <img src="img/logo.png" height="100" width="auto" alt="">
         </a></div>
     <div class="sidebar-wrapper">
         <ul class="nav">
-            <li class="nav-item active  ">
-                <a class="nav-link" href="back">
-                    <i class="material-icons">Profile</i>
-                    <p>Profile</p>
-                </a>
-            </li>
             <li class="nav-item ">
-                <a class="nav-link" href="./user.html">
-                    <i class="material-icons">person</i>
-                    <p>User Profile</p>
-                </a>
-            </li>
-            <li class="nav-item ">
-                <a class="nav-link" href="tables">
+                <a class="nav-link active" href="tables">
                     <i class="material-icons">content_paste</i>
                     <p>Table List</p>
-                </a>
-            </li>
-            <li class="nav-item ">
-                <a class="nav-link" href="./typography.html">
-                    <i class="material-icons">library_books</i>
-                    <p>Typography</p>
-                </a>
-            </li>
-            <li class="nav-item ">
-                <a class="nav-link" href="./icons.html">
-                    <i class="material-icons">bubble_chart</i>
-                    <p>Icons</p>
-                </a>
-            </li>
-            <li class="nav-item ">
-                <a class="nav-link" href="./map.html">
-                    <i class="material-icons">location_ons</i>
-                    <p>Maps</p>
-                </a>
-            </li>
-            <li class="nav-item ">
-                <a class="nav-link" href="./notifications.html">
-                    <i class="material-icons">notifications</i>
-                    <p>Notifications</p>
-                </a>
-            </li>
-            <li class="nav-item ">
-                <a class="nav-link" href="./rtl.html">
-                    <i class="material-icons">language</i>
-                    <p>RTL Support</p>
-                </a>
-            </li>
-            <li class="nav-item active-pro ">
-                <a class="nav-link" href="./upgrade.html">
-                    <i class="material-icons">unarchive</i>
-                    <p>Upgrade to PRO</p>
                 </a>
             </li>
         </ul>
